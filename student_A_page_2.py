91a8ef13ae5b74e8c7d44cae93f134f3526aeabd
char-whit <s4070444@student.rmit.edu.au>
2025-06-07 20:26:19 +1000

    changes (A)

--- student_A_page_2.py
+++ student_A_page_2.py
@@ -6,35 +6,8 @@ def get_page_html(form_data):
     <!DOCTYPE html>
     <html>
     <head>
-        <link rel="stylesheet" href="style.css">
-        <style>
-            .styled-table {
-                border-collapse: collapse;
-                margin: 25px 0;
-                font-size: 0.9em;
-                font-family: sans-serif;
-                min-width: 400px;
-                box-shadow: 0 0 20px rgba(0, 0, 0, 0.15);
-            }
-            .styled-table thead tr {
-                background-color: #009879;
-                color: #ffffff;
-                text-align: left;
-            }
-            .styled-table th,
-            .styled-table td {
-                padding: 12px 15px;
-            }
-            .styled-table tbody tr {
-                border-bottom: 1px solid #dddddd;
-            }
-            .styled-table tbody tr:nth-of-type(even) {
-                background-color: #f3f3f3;
-            }
-            .styled-table tbody tr:last-of-type {
-                border-bottom: 2px solid #009879;
-            }
-        </style>
+        <link rel="stylesheet" href="A_page2.css">
+
     </head>
     <body>
         <div class="header">
@@ -47,9 +20,7 @@ def get_page_html(form_data):
         <div class="topnav">
             <a href="/">Home</a>
             <a href="/mission">Mission Statement</a>
-            <div class="dropdown">
-                <button class="dropbtn">Weather Stations <i class="fa fa-caret-down"></i></button>
-                <div class="dropdown-content">
+           
                     <a href="/weather-stations">Climate Information</a>
                     <a href="/weather-stations-similar">Similar stations</a>
                 </div>
